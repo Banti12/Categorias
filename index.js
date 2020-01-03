20463c3c5a05f288c768d8aad44745bd2ca19e75
Brayan Quirino <dracko.bq@gmail.com>
2020-01-03 01:30:40 +0000

    LaunchRequestHandler

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const https=require('https');
 const bodyParser= require('body-parser');
 const Speech = require ('ssml-builder');
 
+const mensaje = 'Bienvenido a categorias';
+const reprompt = '¿En qué puedo ayudarte?';
+const pause = '<break time="0.3s" />';
+const saber_mas = '¿Quieres saber más?';
+
 //@param app es lo que va a ejecutar la app
 const app=express();
 const directoryToServe='client'
@@ -27,30 +32,48 @@ app.use(bodyParser.urlencoded({limit: '100mb', extended: true}));
 app.listen(8080);
 // crea otra direccion que el cliente solicita en este caso, /categorias
 app.post('/categorias' , (req, res,next) => {
-        //console.log(req.body);
-        var speech = new Speech();
-        speech.say('Bienvenido a la información de categorías')
-        .pause('1s')
-        .say('Aun no funciono bien jajajaja Oscar adenuar esta con migo ')
-        .pause('1s')
-        .say('Me hace falta poco')
-        .pause('1s')
-        var outputSpeech = speech.ssml(true);
-        res.json({
-                "version": "1.0",
-                "sessionAttributes":{
-                },
-                "response":{
-                "outputSpeech":{
-                "type":"SSML",
-                "ssml": `<speak>${outputSpeech}</speak>`
-                },
-                "shouldEndSession": false
-                }
-        })
-        //le damos un siguiente para que no se quede pasmada la maquina
-        next();
+	if (req.body.request.type === 'LaunchRequest') {
+    		res.json(bienvenida());
+	}    	
 });
+
+function bienvenida() {
+    const tempOutput = mensaje + pause;
+    const speechOutput = tempOutput;
+    const more = 'Te puedo dar información sobre categorías';
+console.log("hola1");
+    return buildResponsebienvenida(speechOutput, false, more); 
+  }
+
+function buildResponsebienvenida (speechText, shouldEndSession, cardText) {
+  
+    const mensaje = "<speak>" + speechText + "</speak>"
+console.log("entre");
+    var jsonObj = {
+        "version": "1.0",
+        "response": {
+	    "shoulEndSession": shouldEndSession,
+            "outputSpeech": {
+            "type": "SSML",
+            "ssml": mensaje
+            },
+            "card": {
+            "type": "Simple",
+            "title": "Bienvenida",
+            "content": cardText
+            },
+            "reprompt": {
+            "outputSpeech": {
+                "type": "SSML",
+                "ssml": cardText
+            }
+            },
+            //"shouldEndSession": shouldEndSession
+        }
+    }
+    console.log("hola2!");
+    return jsonObj
+}
 
 //finalmente creamos el servidor httpS que usa a app
 https.createServer(httpsOptions,app).listen(port,function(){
