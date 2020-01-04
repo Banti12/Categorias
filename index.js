18264c7dc29958e44c78bc45d32e44294697c4c7
Brayan Quirino <dracko.bq@gmail.com>
2020-01-04 09:32:26 +0000

    ayuda

--- index.js
+++ index.js
@@ -5,10 +5,14 @@ const https=require('https');
 const bodyParser= require('body-parser');
 const Speech = require ('ssml-builder');
 
-const mensaje = 'Bienvenido a categorias';
+var isFisrtTime = true;
+let nombreIntent = "" ;
+const nombre = 'Categorias';
+const mensaje = 'Bienvenido a categorías';
 const reprompt = '¿En qué puedo ayudarte?';
 const pause = '<break time="0.3s" />';
 const saber_mas = '¿Quieres saber más?';
+const Mensajeayuda = 'Hola mi nombre es Alexa, soy una herramienta que te podrá ayudar para la obtención de los datos de categorías.';
 
 //@param app es lo que va a ejecutar la app
 const app=express();
@@ -34,21 +38,108 @@ app.listen(8080);
 app.post('/categorias' , (req, res,next) => {
 	if (req.body.request.type === 'LaunchRequest') {
     		res.json(bienvenida());
-	}    	
+		isFisrtTime = false;
+	}else if (req.body.request.type === 'IntentRequest') { 
+		switch (req.body.request.intent.name) {
+			case 'AMAZON.HelpIntent':
+			res.json(ayuda());
+			break;
+			case 'Si':
+			nombreIntent = req.body.request.intent.name;
+			console.log(nombreIntent);
+			let resultado = si();
+			res.json(resultado);
+			break;
+		}
+	}   	
 });
 
+/* Funciones que se utilizaran para los Intent request */
+
 function bienvenida() {
+    if (!isFisrtTime) {
+      mensaje = '';
+    }
     const tempOutput = mensaje + pause;
     const speechOutput = tempOutput;
     const more = 'Te puedo dar información sobre categorías';
-console.log("hola1");
-    return buildResponsebienvenida(speechOutput, false, more); 
+    return buildResponseWithRepromt(speechOutput, false, 'Bienvenido a Alexa ', more);
+  }
+
+function ayuda() {
+    const tempOutput = Mensajeayuda + pause;
+    const speechOutput = tempOutput;
+    const reprompt = '¿Te gustaría saber cómo preguntar para obtener la información?';
+    const cardText = 'Te encuentras en el menu de ayuda';
+    return buildResponseWithRepromt(speechOutput, false, cardText, reprompt);
+  }
+
+async function si(){
+  var jsonObj;
+  const speechOutput = "";
+  const reprompt = "";
+  if(nombreIntent =='AMAZON.HelpIntent'){
+      speechOutput = "Existen 3 formas de preguntar" + pause + "La primera consiste en decir categorias " + pause ;
+      reprompt = '';
+      const cardText = 'Formas de preguntar';
+      jsonObj = buildResponseWithRepromt(speechOutput, false, cardText , reprompt);
+  }
+  return await jsonObj;
+}
+
+/* Funciones build response: ayudaran a formatear la salida a tipo alexa */
+
+function buildResponse(speechText, shouldEndSession, cardText) {
+    const speechOutput = "<speak>" + speechText + "</speak>"
+    var jsonObj = {
+      "version": "1.0",
+      "response": {
+        "shouldEndSession": shouldEndSession,
+        "outputSpeech": {
+          "type": "SSML",
+          "ssml": speechOutput
+        },
+        "card": {
+          "type": "Simple",
+          "title": nombre,
+          "content": cardText,
+          "text": cardText
+        }
+      }
+    }
+    return jsonObj
+  }
+
+function buildResponseWithRepromt(speechText, shouldEndSession, cardText, reprompt) { 
+    const speechOutput = "<speak>" + speechText + "</speak>"
+    var jsonObj = {
+       "version": "1.0",
+       "response": {
+        "shouldEndSession": shouldEndSession,
+         "outputSpeech": {
+           "type": "SSML",
+           "ssml": speechOutput
+         },
+       "card": {
+         "type": "Simple",
+         "title": nombre,
+         "content": cardText,
+         "text": cardText
+       },
+       "reprompt": {
+         "outputSpeech": {
+           "type": "PlainText",
+           "text": reprompt,
+           "ssml": reprompt
+         }
+       }
+     }
+   }
+    return jsonObj
   }
 
 function buildResponsebienvenida (speechText, shouldEndSession, cardText) {
-  
     const mensaje = "<speak>" + speechText + "</speak>"
-console.log("entre");
     var jsonObj = {
         "version": "1.0",
         "response": {
@@ -67,11 +158,9 @@ console.log("entre");
                 "type": "SSML",
                 "ssml": cardText
             }
-            },
-            //"shouldEndSession": shouldEndSession
+            }
         }
     }
-    console.log("hola2!");
     return jsonObj
 }
 
